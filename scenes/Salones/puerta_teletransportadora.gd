bbf7cfde33b19a8ca69415d3eb9420bfd010b3d3
Mariana <marianagiraldotabares@gmail.com>
2026-04-13 13:33:16 -0500

    Creada ventana donde se muestra el texto de cada expediente, con un botón para cerrar la ventana. Además, se han agregado interacciones para abrir esta ventana al hacer clic en cada expediente. También se ha eliminado la escena de transición y se han actualizado las escenas de los salones para incluir las puertas teletransportadoras que llevan a los diferentes salones.

--- scenes/Salones/puerta_teletransportadora.gd
+++ scenes/Salones/puerta_teletransportadora.gd
@@ -3,12 +3,12 @@ extends Area2D
 # 🌍 Esta línea crea una casilla en el Inspector para elegir el salón
 @export_file("*.tscn") var escena_destino
 
-# Esta función se activará cuando conectemos la señal en el siguiente paso
 func _on_body_entered(body: Node2D):
-	# Verificamos que sea la psicóloga y que hayamos elegido un destino
+	# Verificamos que sea la psicologa (minúscula como la tienes)
 	if body.name == "psicologa":
 		if escena_destino != "":
-			# Llamamos al Autoload "Transicion" y su función
-			Transicion.cambiar_escena(escena_destino)
+			print("Cambiando a escena: ", escena_destino)
+			# USAMOS EL COMANDO DIRECTO:
+			get_tree().change_scene_to_file(escena_destino)
 		else:
 			print("¡Error! No has elegido a qué salón va esta puerta en el Inspector")
