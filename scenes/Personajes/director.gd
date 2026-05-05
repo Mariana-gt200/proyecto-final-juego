7751b657fc40e60b57f8f7f5da3bbff15dc7e296
Sunny <silvanalopezgallego1@gmail.com>
2026-05-05 10:31:58 -0500

    inicio y fin de dialogos

--- scenes/Personajes/director.gd
+++ scenes/Personajes/director.gd
@@ -2,13 +2,26 @@ extends Area2D
 
 @onready var exclamation_mark= $ExclamationMark
 
+var is_dialogue_active=false
 var is_psicologa_close=false
 const PSICOLOGA_EJEMPLO=preload("res://dialogos/psicologaEjemplo.dialogue")
 
 
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
+
+
+
+func on_dialogue_started(dialogue):
+	is_dialogue_active=true
+	
+func on_dialogue_ended(dialogue):
+	is_dialogue_active=false
+
 
 func _physics_process(delta: float) -> void:
-	if is_psicologa_close and Input.is_action_just_pressed("interactuar"):
+	if is_psicologa_close and Input.is_action_just_pressed("interactuar") and not is_dialogue_active:
 		DialogueManager.show_dialogue_balloon(PSICOLOGA_EJEMPLO)
 
 
