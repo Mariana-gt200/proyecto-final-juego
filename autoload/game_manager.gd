e1b4afcaa4a5651b52a0ffc460f40d47588a7351
Sunny <silvanalopezgallego1@gmail.com>
2026-05-14 13:21:28 -0500

    dialogos de carlos y erika ya escritos
    falta hacerles instancia y el dialogo de jessica

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -2,6 +2,12 @@ extends Node
 
 var is_dialogue_active=false
 var conocio_al_director=false
+var conocio_a_jessica=false
+var conocio_a_carlos=false
+var conocio_a_erika=false
+var respuesta_jessica=false
+var respuesta_carlos=false
+var respuesta_erika=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
