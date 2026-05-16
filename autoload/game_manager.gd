5de4853d054dc936526c9e62ddc8bcaae4920a9f
Sunny <silvanalopezgallego1@gmail.com>
2026-05-16 18:33:39 -0500

    ediciones en los dialogos

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -1,14 +1,16 @@
 extends Node
 
-var is_dialogue_active=false
-var conocio_al_director=false
-var conocio_a_jessica=false
-var conocio_a_carlos=false
-var conocio_a_erika=false
-var respuesta_jessica=false
-var respuesta_carlos=false
-var respuesta_erika=false
-# Called when the node enters the scene tree for the first time.
+var is_dialogue_active:bool=false
+var conocio_al_director:bool=false
+var conocio_a_jessica:bool=false
+var conocio_a_carlos:bool=false
+var conocio_a_erika:bool=false
+var respuesta_jessica:bool=false
+var respuesta_carlos:bool=false
+var respuesta_erika:bool=false
+
+
+
 func _ready() -> void:
 	pass # Replace with function body.
 
---
+++ scenes/Personajes/carlos.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@onready var exclamation_mark= $ExclamationMark
+
+
+
+var is_psicologa_close=false
+const carlos=preload("res://dialogos/carlos.dialogue")
+
+
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
+
+func _on_area_entered(area: Area2D) -> void:
+	exclamation_mark.visible= true
+	is_psicologa_close=true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	exclamation_mark.visible= false
+	is_psicologa_close=false
+
+
+func on_dialogue_started(dialogue):
+	GameManager.is_dialogue_active=true
+	
+func on_dialogue_ended(dialogue):
+	GameManager.is_dialogue_active=false
+
+
+func _physics_process(delta: float) -> void:
+	if is_psicologa_close and Input.is_action_just_pressed("hablar") and not GameManager.is_dialogue_active:
+		DialogueManager.show_dialogue_balloon(carlos)
