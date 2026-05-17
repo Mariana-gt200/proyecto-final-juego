fed222f7e4b0a4a94eb6d2d9de7c74defb3812e6
Sunny <silvanalopezgallego1@gmail.com>
2026-05-16 19:12:07 -0500

    finales ya hechos
    falta remodelacion de psicologa y barra de misiones

--- scenes/finales/finales.gd
+++ scenes/finales/finales.gd
@@ -7,13 +7,13 @@ var respuesta_carlos=GameManager.respuesta_carlos
 var respuesta_erika=GameManager.respuesta_erika
 
 
-
-# Called when the node enters the scene tree for the first time.
-func entrar_a_finales():
-	if conoce_carlos and conoce_erika and conoce_jessica:
+func _ready() -> void:
+	
 		if respuesta_carlos and respuesta_erika and respuesta_jessica== true:
 			get_tree().change_scene_to_file("res://scenes/finales/finalBueno.tscn")
 		elif respuesta_carlos or respuesta_erika or respuesta_jessica==true:
 			get_tree().change_scene_to_file("res://scenes/finales/finalMedio.tscn")
 		else:
 			get_tree().change_scene_to_file("res://scenes/finales/finalMalo.tscn")
+
+	
