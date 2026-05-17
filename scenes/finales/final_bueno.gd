b9e6d51fe459364d503b1c0a0253bcf0a0119ae5
Sunny <silvanalopezgallego1@gmail.com>
2026-05-17 15:40:19 -0500

    .

--- scenes/finales/final_bueno.gd
+++ scenes/finales/final_bueno.gd
@@ -1,9 +1,11 @@
 extends Node2D
-
+@onready var musica=$AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	musica.play()
+	MusicManager.stop_music()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
